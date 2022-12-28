9490b63487fa874a678d726f412904773671e9e9
ShadowGuard471 <shadowguardofficial@gmail.com>
2022-12-28 17:28:16 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function Overlay() {
         </a>
       </div>
       <p style={{ position: 'absolute', bottom: 30, left: 90, fontSize: '12px' }}>
-        WEB3 CREATIVE STUDIO STARTUPppp
+        WEB3 CREATIVE STUDIO STARTUPppplas
         <br />@ C Y B E R V E R S E
       </p>
       {/* <a href="https://twitter.com/" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px'}}>
